da63c707bf07bb17fa748025f0f2337b082963d1
kevinalbert7 <20ko@live.fr>
2021-10-12 16:00:14 +0200

    todolist/html/css/js

---
+++ index.js
@@ -0,0 +1,12 @@
+// ----------Fonction nouvelle tâche----------------
+
+var task = document.getElementById("name")
+// var taskArray = document.getAttribute("newTask")
+var taskHTML = document.getElementById("tâches")
+
+function onTaskSubmit() {
+    // taskHTML.innerHTML = ""
+    
+    console.log(task.value)
+}
+
