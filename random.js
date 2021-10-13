8dba59c68733219e9761c3f257b9c692b4ccd3bb
HeleneSoubeyrand <helenesoubi@gmail.com>
2021-10-13 17:22:56 +0200

    function random

--- random.js
+++ random.js
@@ -1,1 +1,9 @@
-var randomArray=["Lire le dictionnaire en entier", "Trier par taille toutes les vis de la boîte à outils","Lancer un faux débat sur Twitter", "Supprimer les mails inutiles","Tester tous les stylos de la maison","" ]
+var randomArray=["Lire le dictionnaire en entier", "Trier par taille toutes les vis de la boîte à outils","Lancer un faux débat sur Twitter", "Supprimer les mails inutiles","Tester tous les stylos de la maison","Laver les rideaux","Acheter un chat pour combler ma solitude" ]
+
+function randomTask() {
+var min = 0
+var max = 5
+var random = Math.floor(Math.random() * (max - min + 1) + min)
+
+return array[random]
+}
