8bf2e340f04e628cb08874d19cfb4e323d5092f2
HeleneSoubeyrand <helenesoubi@gmail.com>
2021-10-13 19:47:18 +0200

    random.js MAJ liste

--- random.js
+++ random.js
@@ -1,8 +1,8 @@
-var randomArray = ["Lire le dictionnaire en entier", "Trier par taille toutes les vis de la boîte à outils","Lancer un faux débat sur Twitter", "Supprimer les mails inutiles","Tester tous les stylos de la maison","Laver les rideaux","Acheter un chat pour combler ma solitude" ]
+var randomArray = ["Lire le dictionnaire en entier", "Trier par taille toutes les vis de la boîte à outils","Lancer un faux débat sur Twitter", "Supprimer les mails inutiles","Tester tous les stylos de la maison","Pousser mémé dans les orties","Assembler toutes les paires de chaussettes", "Me faire cuire un oeuf"]
 
 function random() {
 var min = 0
-var max = 5
+var max = 7
 var random = Math.floor(Math.random() * (max - min + 1) + min)
 
 return randomArray[random]
