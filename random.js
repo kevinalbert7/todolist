18b8464935c77b8fae2debc2fd14d7d28ac3a4ef
HeleneSoubeyrand <helenesoubi@gmail.com>
2021-10-13 19:57:40 +0200

    css button

--- random.js
+++ random.js
@@ -1,4 +1,4 @@
-var randomArray = ["Lire le dictionnaire en entier", "Trier par taille toutes les vis de la boîte à outils","Lancer un faux débat sur Twitter", "Supprimer les mails inutiles","Tester tous les stylos de la maison","Pousser mémé dans les orties","Assembler toutes les paires de chaussettes", "Me faire cuire un oeuf"]
+var randomArray = ["Apprendre le dictionnaire en entier", "Trier par taille toutes les vis de la boîte à outils","Lancer un faux débat sur Twitter", "Supprimer les mails inutiles","Tester tous les stylos de la maison","Pousser mémé dans les orties","Assembler toutes les paires de chaussettes", "Me faire cuire un oeuf"]
 
 function random() {
 var min = 0
