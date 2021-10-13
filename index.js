c8c84386bfa2128b5a6e8e258b899c4a0ff34235
kevinalbert7 <20ko@live.fr>
2021-10-13 17:23:59 +0200

    rename id menu

--- index.js
+++ index.js
@@ -24,39 +24,36 @@ function editStatus(index) {
     list.innerHTML = ``
   
     tasks.forEach(function(task, index) {
-      list.innerHTML = list.innerHTML + `
+        list.innerHTML = list.innerHTML + `
       
-      <div class="line">           
+    <div class="line">           
             
-      <div id="doingSticker">
-          <img src="img/logo_Check.svg" alt="">
-          <div class="statusLabel ${task.status}Color">
-              <p>${task.status}</p>
-          </div>
-      </div>
+        <div class="statusSticker" id="doingSticker">
+            <img src="img/logo_Check.svg" alt="">
+            <div class="statusLabel ${task.status}Color"> /* Couleur de l'étiquette */
+                <p>${task.status}</p>  /* Nom de l'étiquette */
+            </div>
+        </div>
       
       
-      <div class="list">   
-          <p>${task.value} </p>
-      </div>                       
+        <div class="list">   
+            <p>${task.value} </p>
+        </div>                       
       
-      <div class="modifyMenu">
-          <ul id="menu-demo2">
-              <li><a id="edit" href="#">Modifier</a>
-                  <ul>
-                      <li onclick="editStatus('Todo')">To do</li>
-                      <li onclick="editStatus('Doing')">Doing</li>
-                      <li onclick="editStatus('Done')">Done</li>
-                  </ul>
-              </li>
-          </ul>
-      </div>
+        <div class="modifyMenu">
+        <div><a id="edit" href="#">Modifier</a></div>
+            <ul id="menu-demo2">
+                <li onclick="editStatus('Todo')">To do</li>
+                <li onclick="editStatus('Doing')">Doing</li>
+                <li onclick="editStatus('Done')">Done</li>
+            </ul>
+        </div>
       
-      <div>
-          <button class="deleteButton">
-              <p>Delete</p>
-          </button>
-      </div>
+        <div>
+            <button class="deleteButton">
+            <p>Delete</p>
+            </button>
+        </div>
 
   </div>
   `
