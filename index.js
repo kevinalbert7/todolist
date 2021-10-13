371f85512b525a91a6986710562b3143169395ea
HeleneSoubeyrand <helenesoubi@gmail.com>
2021-10-13 15:59:38 +0200

    function edit en cours

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var list = document.getElementById("list")
 var tasks = []
 
+
 function onTaskSubmit() {
     var input = document.getElementById("name").value
     var task = {
@@ -10,44 +11,64 @@ function onTaskSubmit() {
         status: "To do",
     }
     tasks.push(task)
+    displayList()
+}       
 
-    list.innerHTML = ""
-    
-    tasks.forEach(function(task) {
-        list.innerHTML = list.innerHTML + `
+function editStatus(index) {
+    tasks[index].status = "doing"
+
         
-        <div class="line">           
-        
-        <div id="status" class="status">
-        </div>
-        
-        <div class="list">   
-            <p>${task.value} </p>
-        </div>                       
-        
-        <div class="modifyMenu">
-            <ul id="menu-demo2">
-                <li><a href="#">Modifier</a>
-                    <ul>
-                        <li><a href="#">To do</a></li>
-                        <li><a href="#">Doing</a></li>
-                        <li><a href="#">Done</a></li>
+
+    function displayList() {
+        list.innerHTML = ""
+
+        tasks.forEach(function(task) {
+            list.innerHTML = list.innerHTML + `
+            
+            <div class="line">           
+            
+                <div id="doingSticker">
+                    <img src="img/logo_Check.svg" alt="">
+                    <div class="statusLabel ${task.status}Color">
+                        <p>${task.status}</p>
+                    </div>
+                </div>
+                
+                
+                <div class="list">   
+                    <p>${task.value} </p>
+                </div>                       
+                
+                <div class="modifyMenu">
+                    <ul id="menu-demo2">
+                        <li><a href="#">Modifier</a>
+                            <ul>
+                                <li onclick="editStatus('Todo')">To do</li>
+                                <li onclick="editStatus('Doing')">Doing</li>
+                                <li onclick="editStatus('Done')">Done</li>
+                            </ul>
+                        </li>
                     </ul>
-                </li>
-            </ul>
-        </div>
-        
-        <div>
-            <button class="deleteButton">
-                <a href="#">Delete</a>
-            </button>
-        </div>
-
-    </div>
-    `
-    })
-        
+                </div>
+                
+                <div>
+                    <button class="deleteButton">
+                        <p>Delete</p>
+                    </button>
+                </div>
+    
+            </div>
+            `
+        })
     }
 
+    function editValue() {
+
+    }
+
+    
 
 
+    function delete() {
+
+    }
