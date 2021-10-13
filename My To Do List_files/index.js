37415cdb3475e1aced3b079bd0b1006ba6c0931d
HeleneSoubeyrand <helenesoubi@gmail.com>
2021-10-13 11:19:17 +0200

    11:18 maj section taskbloc et html line

---
+++ My To Do List_files/index.js
@@ -0,0 +1,14 @@
+// ----------Fonction nouvelle tâche----------------
+
+var task = document.getElementById("name")
+var taskHTML = document.getElementById("tâches")
+var taskArray = []
+
+
+function onTaskSubmit() {
+    taskHTML.innerHTML
+    
+    console.log(task.value)
+}
+
+
